69126f7e667a9e0d5104b8044a1a4c1108628ecc
Owen Abbott <ocabbott@gmail.com>
2020-01-27 21:49:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,6 +34,7 @@ def update_counting_hash(hash, key)
     hash[key]=1
   else
     hash+=1
+  end
   return hash
   # given a hash an a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
